4d53ee98ea6fc04f7351339b4729b79df487f8bb
unknown <kusumarizky65@gmail.com>
2024-04-29 07:39:01 +0700

    Menambahkan api Mahasiswa

--- modul9_1302220013/Controllers/WeatherForecastController.cs
+++ modul9_1302220013/Controllers/WeatherForecastController.cs
@@ -4,30 +4,62 @@ namespace modul9_1302220013.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class MahasiswaController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        public static List<string> matkul = new List<string>()
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "PBO", "KPL", "Basdat"
+        };
+        
+        private static List<Mahasiswa> _mahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa { Nama = "Rizky Kusuma Nugraha", Nim = "1302220013", course = matkul, year = 20 },
+            new Mahasiswa { Nama = "Muhammad Rizqi Fadhillah", Nim = "1302220020", course = matkul, year = 20 },
+            new Mahasiswa { Nama = "Reinhard E", Nim = "13022200...", course = matkul, year = 20 }
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        [HttpGet]
+        public ActionResult<IEnumerable<Mahasiswa>> GetMahasiswa()
         {
-            _logger = logger;
+            return _mahasiswa;
         }
-
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet("{index}")]
+        public ActionResult<Mahasiswa> GetMahasiswaByIndex(int index)
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            if (index < 0 || index >= _mahasiswa.Count)
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                return NotFound();
+            }
+
+            return _mahasiswa[index];
         }
+
+        [HttpPost]
+        public ActionResult PostMahasiswa(Mahasiswa mahasiswa)
+        {
+            _mahasiswa.Add(mahasiswa);
+            return CreatedAtAction(nameof(GetMahasiswaByIndex), new { index = _mahasiswa.Count - 1 }, mahasiswa);
+        }
+
+        [HttpDelete("{index}")]
+        public ActionResult DeleteMahasiswa(int index)
+        {
+            if (index < 0 || index >= _mahasiswa.Count)
+            {
+                return NotFound();
+            }
+
+            _mahasiswa.RemoveAt(index);
+            return NoContent();
+        }
+
+    }
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+        public List<string> course { get; set; }
+        public int year { get; set; }
+
     }
 }
--- modul9_1302220013/obj/Debug/net8.0/modul9_1302220013.AssemblyInfo.cs
+++ modul9_1302220013/obj/Debug/net8.0/modul9_1302220013.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul9_1302220013")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d09ad91643f58528461dc351444f959e9485d2ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul9_1302220013")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul9_1302220013")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
